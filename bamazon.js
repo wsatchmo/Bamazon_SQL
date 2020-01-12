5a0240272e75df9843601407bcd8cf3b843ce1a7
wsatchmo <wsatcher@outlook.com>
2020-01-11 16:25:15 -0800

    Functional app, updates database

--- bamazon.js
+++ bamazon.js
@@ -30,8 +30,7 @@ function postProducts(){
             console.log(`Item ID: ${res[i].item_id} | Item: ${res[i].product_name} | Department: ${res[i].department_name} | Price: ${res[i].price} | Stock: ${res[i].stock_quantity}`);
         }
         afterDisplay();
-    });
-    
+    });   
 }
 
 function afterDisplay() {
@@ -40,13 +39,12 @@ function afterDisplay() {
         name: "product_id",
         type: "number",
         message: "What is the ID of the product you would like to search?"
-        //ADD A VALIDATE SO IT HAS TO BE IN THE DATABASE
         })
         .then(function(answer1) {
         // based on their answer, let them know --
         connection.query("SELECT item_id FROM products WHERE ?", {item_id: answer1.product_id}, function(err, res) {
             if (err) throw err;
-            console.log("PRODUCT ID: ", res[0].item_id); // Log all results 
+            console.log("PRODUCT ID: ", res[0].item_id, "ITEM: ", res[0].product_name); // Log all results 
             if (res.length === 0){ //There is not a product by that ID
                 console.log("INVALID SELECTION -- EXITING");
                 connection.end();
@@ -65,18 +63,36 @@ function selectAmount(answer1){
         message: "How many units would you like to order?"
         })
         .then(function(answer2) {
-        // based on their answer, let them know --
+        // based on their answer--
         connection.query("SELECT stock_quantity FROM products WHERE ?", {item_id: answer1.product_id}, function(err, res) {
             if (err) throw err;
-            console.log("Quantity AMT.: ", res[0].stock_quantity); // Log all results 
+            var stockAmt = res[0].stock_quantity;
+            console.log("Quantity AMT.: ", stockAmt); // Log all results 
             console.log("Order AMT.: ", answer2.amount); // Log desired quantity 
-            if (res[0].stock_quantity < answer2.amount){ //EITHER - There is not enough quantity
-                console.log("NOT ENOUGH STOCK -- EXITING");
-                connection.end();
+            if (stockAmt < answer2.amount){ //EITHER - There is not enough quantity
+                console.log("NOT ENOUGH STOCK -- TRY AGAIN");
+                selectAmount(answer1);
             } else {
-                //OR - That their order has been placed; update quantity & re-print products 
+                console.log("Your order has been placed -- thank you for shopping as Bamazon!"); //OR - That their order has been placed
+                var newStock = stockAmt - answer2.amount; // update quantity
+                console.log("Stock updated from " + stockAmt + " to " + newStock);
+                connection.query("UPDATE products SET stock_quantity=" + newStock + " WHERE item_id=" + answer1.product_id, function(err, res) {
+                    if (err) throw err;
+                });
+                inquirer
+                .prompt({
+                    name: "exiter",
+                    type: "list",
+                    choices: ["Yes", "No"],
+                    message: "Would you like to order something else?"
+                }).then(function(answer) {
+                    if (answer.exiter === "Yes"){
+                        postProducts();
+                    } else if (answer.exiter === "No"){
+                        connection.end();
+                    }
+                });
             }  
         });
-       
     });
 }
